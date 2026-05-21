fdc9fb8a942d385c1215fe8e70f9d35d5e330c83
koryojohogijutsu <167965786+koryojohogijutsu@users.noreply.github.com>
2026-05-22 07:30:16 +0900

    Update page.tsx

--- app/admin/classes/page.tsx
+++ app/admin/classes/page.tsx
@@ -1,12 +1,8 @@
 "use client";
+import { supabase } from "@/lib/supabase";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
 
 export default function ClassesAdminPage() {
   const router = useRouter();
